31672c58880d93e337158b655f087eee3dc88643
fish-bluff-cats <1159767295@qq.com>
2023-11-05 22:34:24 +0800

    refactor:finish the project #02

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,13 +5,14 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
@@ -35,3 +36,4 @@ mod tests {
         assert_eq!(foo_if_fizz("literally anything"), "baz")
     }
 }
+
